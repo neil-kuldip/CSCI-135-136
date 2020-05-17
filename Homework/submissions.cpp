e113230f48f836249d9042de52003c391a9d7254
Neil Kuldip <56613674+neil-kuldip@users.noreply.github.com>
2020-05-17 00:30:14 -0400

    Create submissions.cpp

---
+++ Homework/submissions.cpp
@@ -0,0 +1,19 @@
+/*
+Author: Neil Kuldip
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E.17 - 3 Names
+
+This program prints the names of my three favorite movies, on three seperate lines.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Knives Out" << endl;
+	cout << "Moonlight" << endl;
+	cout << "Avengers: Endgame" << endl;
+	return 0;
+}
